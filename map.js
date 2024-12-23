e753f301a8a853d83eabc0ee9b533a36425d571c
Praveen Kumar <183719173+praveenpkv@users.noreply.github.com>
2024-12-23 17:05:58 +0530

    updated truthValueOf

--- map.js
+++ map.js
@@ -21,10 +21,11 @@ const firstCharactersOf = function (strings) {
 // truth values of [0, 1, 2, 3] => [false, true, true, true]
 // Assume non-zero numbers are true, and zero is false
 const truthValuesOf = function (numbers) {
-  return numbers.map((number) => Math.abs(number) > 0);
+  return numbers.map((number) => Math.abs(number) != 0);
 };
 
 // reverse strings of ["hello", "world"] => ["olleh", "dlrow"]
+
 const reversedStringsOf = function (strings) {};
 
 // double letters of ["cat", "dog", "bat"] => ["ccaat", "ddoog", "bbaatt"]
